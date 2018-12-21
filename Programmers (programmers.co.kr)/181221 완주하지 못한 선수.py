0feebd528129214417c2cd5070915740b90bdfdb
Seohyun Choi <36473249+seohyunchoi123@users.noreply.github.com>
2018-12-21 17:00:31 +0900

    Add files via upload

--- Programmers (programmers.co.kr)/181221 완주하지 못한 선수.py
+++ Programmers (programmers.co.kr)/181221 완주하지 못한 선수.py
@@ -8,3 +8,33 @@
 # completion의 길이는 participant의 길이보다 1 작습니다.
 # 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
 # 참가자 중에는 동명이인이 있을 수 있습니다.
+# 입출력 예
+# participant	completion	return
+# [leo, kiki, eden]	[eden, kiki]	leo
+# [marina, josipa, nikola, vinko, filipa]	[josipa, filipa, marina, nikola]	vinko
+# [mislav, stanko, mislav, ana]	[stanko, ana, mislav]	mislav
+# 입출력 예 설명
+# 예제 #1
+# leo는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+#
+# 예제 #2
+# vinko는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+#
+# 예제 #3
+# mislav는 참여자 명단에는 두 명이 있지만, 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다.
+
+def solution(participant, completion):
+    dic=dict()
+    for person in participant :
+        if person not in dic:
+            dic[person] = 1
+        else:
+            dic[person] += 1
+    for person in completion :
+        dic[person] -= 1
+    idx=0
+    for chk in dic.values():
+        if chk == 1:
+            break
+        idx+=1
+    return list(dic.keys())[idx]
