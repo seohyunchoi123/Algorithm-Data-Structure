761947edc945f84894ba102ace180ebb7779dc42
Seohyun Choi <36473249+seohyunchoi123@users.noreply.github.com>
2019-01-17 16:22:51 +0900

    Add files via upload

---
+++ Hackerrank (www.hackerrank.com)/190117 Non-Divisible Subset.py
@@ -0,0 +1,27 @@
+# Question Link : https://www.hackerrank.com/challenges/non-divisible-subset/problem
+
+from collections import Counter, defaultdict
+
+
+def nonDivisibleSubset(k, S):
+    S = list(map(lambda x : x % k, S))
+    dic = defaultdict(lambda : 0, Counter(S)) # This size is always less than k( < 100).
+    for i in range(1, k//2 + 1):
+        if i == k/2:
+            if dic[i] > 0 :
+                dic[i] = 1 # it should not be more than 2
+        elif i == 0:
+            if dic[0] > 0:
+                dic[0] = 1  # it should not be more than 2
+        elif dic[i] > dic[k-i]:
+            dic[k-i] = 0
+        else:
+            dic[i] = 0
+    result = 0
+    for key in dic.keys():
+        result += dic[key]
+    return result
+
+# Method to Solve : Hash
+# Time Complexity : O(1)
+# Space Complexity : O(n)
