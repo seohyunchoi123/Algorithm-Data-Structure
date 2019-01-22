d22cb34a44caa93f7c87653e1a32180f13d2f1f0
Seohyun Choi <36473249+seohyunchoi123@users.noreply.github.com>
2019-01-22 14:12:17 +0900

    Add files via upload

---
+++ Hackerrank (www.hackerrank.com)/190121 Queen's Attack ll.py
@@ -0,0 +1,40 @@
+
+def queensAttack(n, k, r_q, c_q, obstacles):
+    possible = 2 * (n - 1) # horizontally, vertically
+    possible += (n - max(r_q, c_q)) + (min(r_q, c_q) - 1) # in direction of y = x
+    tmp = r_q + c_q - 1 # in direction of y = -x
+    if tmp > n:
+        tmp = 2 * n - tmp
+    possible += tmp - 1
+    obs_cand = [0]*8 # considering only the closest obstacle to queen for each way of 8 ways.
+    for obstacle in obstacles:
+        r, c = obstacle
+        if r == r_q: # in case of the same row
+            if c > c_q:
+                obs_cand[0] =  max(obs_cand[0], (n - c + 1))
+            else:
+                obs_cand[1] = max(obs_cand[1], c)
+        elif c == c_q: # in case of the same column
+            if r > r_q:
+                obs_cand[2] = max(obs_cand[2], (n - r + 1))
+            else:
+                obs_cand[3] = max(obs_cand[3], r)
+        elif (r - r_q) == (c - c_q): # in case that they are on y = x line together
+            if c > c_q:
+                obs_cand[4] = max(obs_cand[4], (n - max(r, c) + 1))
+            else:
+                obs_cand[5] = max(obs_cand[5], min(r, c))
+        elif (r - r_q) == -(c - c_q): # in case that they are on y = -x line together
+            if r + c <= n:
+                if c > c_q:
+                    obs_cand[6] = max(obs_cand[6],r)
+                else:
+                    obs_cand[7] = max(obs_cand[7], c)
+            else:
+                if c > c_q:
+                    obs_cand[6] = max(obs_cand[6], (n - c + 1))
+                else:
+                    obs_cand[7] = max(obs_cand[7], (n - r + 1))
+    return possible - sum(obs_cand)
+
+print(queensAttack(8, 1, 4, 4 ,[(3,5), (2,6)]))
